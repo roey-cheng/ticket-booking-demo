5d892526a601b884238e4f84e4f2c35cf89abce7
roey-cheng <ruyi.cheng.tech@gmail.com>
2026-07-07 02:54:19 +1200

    Set up Resend configuration

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
+import os
+import resend
+from dotenv import load_dotenv
 from flask import Flask, render_template, request
+
+load_dotenv()
+
+resend.api_key = os.environ["RESEND_API_KEY"]
 
 app = Flask(__name__)
 
@@ -10,7 +17,10 @@ def home():
 def book():
     if request.method == "POST":
         name = request.form["name"]
-        return f"Hello, {name}! Your booking form was received."
+        email = request.form["email"]
+        tickets = request.form["tickets"]
+
+        return f"Hello, {name}! You requested {tickets} ticket(s). We would send confirmation to {email}."
     return render_template("book.html")
 
 if __name__ == "__main__":
